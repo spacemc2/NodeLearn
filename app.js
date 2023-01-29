cf36c118d428a3add82f59184a52338c098d12e7
Miguel Torres <m.torres.940106@gmail.com>
2023-01-29 23:00:24 +0000

    First\

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const {Arr,Arr2} = require("./data.js")
+
+const mult =  Arr.map(elem => elem*elem)
+
+console.log(mult)
---
+++ data.js
@@ -0,0 +1,7 @@
+const Arr = [1, 2, 4]
+const Arr2 = [1, 2, 4]
+
+module.exports = {
+    Arr: Arr,
+    Arr2
+}
